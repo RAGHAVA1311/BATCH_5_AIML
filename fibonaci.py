c15007fdd62d6c4e541e804017d268ae419067bd
P.Veera Raghava Reddy <146912625+RAGHAVA1311@users.noreply.github.com>
2024-02-02 10:56:58 +0000

    assign

---
+++ fibonaci.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    fib_series = [0, 1]
+    while len(fib_series) < n:
+        fib_series.append(fib_series[-1] + fib_series[-2])
+    return fib_series[:n]
+
+n = 15
+result = fibonacci(n)
+print(f"Fibonacci series with the first {n} numbers: {result}")
